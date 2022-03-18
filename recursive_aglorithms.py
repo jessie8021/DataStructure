e72b415171570626463c93af31f3ba8f23cc47a5
hyemi <jessie8021@gmail.com>
2022-03-18 14:51:59 +0900

    재귀적 이진 탐색의 구현 - 피보나치 수열, 순열, 하노이의 탑, 재귀적 이진탐색

--- recursive_aglorithms.py
+++ recursive_aglorithms.py
@@ -1,5 +1,5 @@
 '''
-[피보나치 순열]
+[피보나치 수열]
 인자로 0 또는 양의 정수인 x 가 주어질 때,
 Fibonacci 순열의 해당 값을 구하여 반환하는 함수 solution() 을 완성하세요.
 
@@ -12,7 +12,6 @@ Fn = Fn - 1 + Fn - 2, n >= 2
 재귀적 방법으로도 프로그래밍해 보고,
 반복적 방법으로도 프로그래밍해 보시기 바랍니다.
 '''
-
 def solution(x):
     f0 = 0
     f1 = 1
@@ -27,29 +26,79 @@ def solution(x):
         x -= 1
     return s
 
+'''
+[풀이]                                1️⃣
+                            fibo(4) → def fibo(4):  3
+                                        if 4 < 2: return 4                   ← #1       8️⃣
+                                        return fibo(4-1) + fibo(4-2)         →          def fibo(2):
+                                                                                            if 2 < 2: return 2
+                                                ↓ ↑ #2                                      return fibo(2-1) + fibo(2-2)
+                                    2️⃣                                 
+                                      def fibo(3):                                             ↓ ↑ #1             ↓ ↑ #0      
+                                        if 3 < 2: return 3                          9️⃣                              🔟
+                                        return fibo(3-1) + fibo(3-2)                def fibo(1):                    def fibo(0):
+                                                                                        if 1 < 2: return 1              if 0 < 2: return 0
+                                            ↓ ↑ #1              ↓ ↑ #1
+                 3️⃣                                             6️⃣
+                 def fibo(2):                                   def fibo(1):
+                    if 2 < 2: return 2                              if 1 < 2: return 1
+                    return fibo(2-1) + fibo(2-2) 
+                    
+                                         
+                            ↓ ↑ #1      ↓ ↑ #0
+          4️⃣                         5️⃣
+          def fibo(1):              def fibo(0):
+            if 1 < 2: return 1          if 0 < 2: return 0
+          
 
-
-def solution(x):
-    # print('[' + str(x) + ']')
+'''
+def fibo(x):
     if (x < 2): return x
     return solution(x - 1) + solution(x - 2)
 
-
 '''
-n 개의 서로 다른 원소에서 m 개를 택하는 경우,
+[순열]
+n(5) 개의 서로 다른 원소에서 m(2) 개를 택하는 경우,
 
 도식화 하면...
-1   2   3   4   5
-2   3   4   5
-3   4   5
-4   5
-5
+    1   2   3   4   5
+
+    2   3   4   5
+    3   4   5
+    4   5
+    5
+    ↓   ↓   ↓   ↓      
+    4   3   2   1
+
+    ⌦ 4 + 3 + 2 + 1 = 10
 
 수학식으로 나타내면
 ⎛n⎞          n!          ⎛ n - 1 ⎞      ⎛ n - 1 ⎞ 
      =  ------------- =             + 
 ⎝m⎠      m! (n - m)!     ⎝   m   ⎠      ⎝ m - 1 ⎠
 
+
+[풀이]
+    comb(5,2)
+        
+    return comb(4,2) + comb(4,1)
+                            ↓
+            ⎥           return comb(3,1) + comb(3,0)
+            ⎥                           ↓
+            ⎥                   return comb(2,1) + comb(2,0)
+            ⎥                                  ↓
+            ↓                          return comb(1,1) + comb(1,0)
+    
+    return comb(3,2) + comb(3,1)
+                            ↓
+            ⎥           return comb(2,1) + comb(2,0)
+            ⎥                           ↓
+            ↓                  return comb(1,1) + (1,0)
+    
+    return comb(2,2) + comb(2,1)
+                            ↓
+                        return comb(1,1) + comb(1,0)
+    
 '''
 def comb(n, m):
     # print('[' + str(n) + ', ' + str(m) + ']')
@@ -57,8 +106,9 @@ def comb(n, m):
     if m == 0 : return 1
     return comb(n - 1, m) + comb(n - 1, m - 1)
 
-'''
-하노이의 탑
+# print(comb(5, 2))
+''' 
+[하노이의 탑]
 1. 크기가 다른 원반 n개를 출발점 기둥에서 도착점 기둥로 전부 옮겨야 한다.
 2. 원반은 한 번에 한 개씩만 옮길 수 있다.
 2. 원반을 옮길 때는 한 기둥의 맨 위 원반을 빼내어, 다른 기중의 맨 위로만 옮길 수 있다.
@@ -78,7 +128,6 @@ def comb(n, m):
 4 : a - c
 
 '''
-
 def hanoi(n, from_pos, to_pos, aux_pos):
     # print(n, ' - ', from_pos, to_pos, aux_pos)
     if n == 1:
@@ -90,8 +139,8 @@ def hanoi(n, from_pos, to_pos, aux_pos):
     hanoi(n-1, aux_pos, to_pos, from_pos)
 
 # hanoi(3, 1, 3, 2)
-
 '''
+[재귀적 이진탐색]
 리스트 L 과, 그 안에서 찾으려 하는 원소 x 가 인자로 주어지고, 
 또한 탐색의 대상이 되는 리스트 내에서의 범위 인덱스가 l 부터 u 까지로 (인자로) 정해질 때, 
 x 와 같은 값을 가지는 원소의 인덱스를 리턴하는 함수 solution() 을 완성하세요. 
@@ -129,6 +178,5 @@ def solution(L, x, l, u):
         return solution(L, x, l, mid-1)
     else:
         return solution(L, x, mid+1, u)
-
 # print(solution([2, 5, 7, 9, 11], 7, 0, 4))
 
